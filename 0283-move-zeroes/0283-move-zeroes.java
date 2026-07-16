e05c777832806b49a3f19c2ed50fb7daabfd13b8
aniket-pandey <aniketarunpandey@gmail.com>
2026-07-16 23:12:42 +0530

    Time: 2 ms (92.19%), Space: 47.9 MB (20.29%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i =-1;
+        for(int j = 0 ; j < nums.length; j++){
+            if(nums[j] != 0){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        i++;
+        while(i < nums.length){
+            nums[i] = 0;
+            i++;
+        }
+    }
+}
