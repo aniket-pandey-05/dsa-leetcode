e01b967cf89686b5a4134684e550ab1e0788c282
aniket-pandey <aniketarunpandey@gmail.com>
2026-07-21 15:36:50 +0530

    Time: 2 ms (94%), Space: 63.2 MB (98.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,16 +5,13 @@ class Solution {
         int curSum = 1;
         leftarr[0] = curSum;
         rightarr[nums.length-1] = curSum;
-        int prevSum;
         for(int i = 1 ; i < nums.length ; i++){
-            prevSum = nums[i-1];
-            curSum *= prevSum;
+            curSum *= nums[i-1];
             leftarr[i] = curSum;
         }
         curSum = 1;
         for(int j = nums.length-2 ; j >= 0 ; j--){
-            prevSum = nums[j+1];
-            curSum *= prevSum;
+            curSum *= nums[j+1];
             rightarr[j] = curSum;
         }
         for(int i = 0 ; i < nums.length ; i++){
