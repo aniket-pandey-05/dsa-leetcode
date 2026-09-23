154aed0a3133f9d5bf0aa7f990c1632480d4b9d2
aniket-pandey <aniketarunpandey@gmail.com>
2026-09-23 07:53:29 +0530

    Time: 1 ms (99.7%), Space: 43 MB (9.02%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            long ans = (long)mid*mid;
+            if(ans <= x) start = mid+1;
+            else end = mid-1;
+        }
+        return end;
+    }
+}
